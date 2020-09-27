cc000c53c123a5ee1ee1a12b0f7ffa5ff894ccd9
iroquoispliskin <41455185+iroquoispliskin@users.noreply.github.com>
2020-09-27 14:14:46 -0400

    Add files via upload

---
+++ Billboardtop100weekly.py
@@ -0,0 +1,35 @@
+#billboard charts api (https://github.com/guoguo12/billboard-charts), and 
+#spotipy (https://spotipy.readthedocs.io/en/latest/)
+
+import billboard
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from config import CLIENT_ID, CLIENT_SECRET
+
+client_credentials_manager = SpotifyClientCredentials(CLIENT_ID, CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def chartdata():
+    """Creates easily traversible chart list"""
+    chart = billboard.ChartData('hot-100')
+    chart_data = []
+    for song in chart:
+        song_data = (song.title, song.artist)
+        chart_data.append(song_data)
+    
+    return chart_data
+
+
+def spotify_search():
+    """returns track ids for each song"""
+    scope = 'playlist-modify-private'
+    chart = chartdata()
+    track_list = []
+    #find a way to get track IDS
+    for track in chart[:10]:
+        results = sp.search(q='artist:' + track[1] + ' track:' + track[0],
+                                 type='track')
+        print(results)
+
+
+spotify_search()
---
+++ config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 27 12:58:28 2020
+
+@author: truet
+"""
+CLIENT_ID = "b73a66085e644b49a7138555c483add9"
+CLIENT_SECRET = "37d1b697b218464eb53c05ff7179e239"
+
